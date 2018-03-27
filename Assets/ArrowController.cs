411b38fbcffa8681f617a172f72a1ceb39a374ef
Iguchi Tomokatsu <iguchi.tomokatsu@designone.jp>
2018-03-27 18:23:29 +0900

    first

---
+++ Assets/ArrowController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour {
+
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		this.player = GameObject.Find ("player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 落下
+		transform.Translate (0, -0.1f, 0);
+
+		// 地面下で消す
+		if (transform.position.y < -5.0f) {
+			// gameObject は自分自身
+			// Destroyは引数にうけたものを消す
+			Destroy (gameObject);
+		}
+
+		// 当たり判定
+		Vector2 p1 = transform.position; // 矢自身
+		Vector2 p2 = this.player.transform.position; // プレイヤ-
+		Vector2 dir = p1 - p2;
+		float d = dir.magnitude; // 2点間の距離を求める
+		float r1 = 0.5f; // 矢の半径
+		float r2 = 1.0f; // プレイヤーの半径
+
+		// 両方の半径を足した数よりも小さいということは、当たったということ。
+		if (d < r1 + r2) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/ArrowGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowGenerator : MonoBehaviour {
+
+	// Outlet接続
+	// => publicが必要!
+	public GameObject arrowPrefab;
+	float span = 1.0f; // 1秒間隔
+	float delta = 0; // 経過時間
+		
+	// Update is called once per frame
+	void Update () {
+		this.delta += Time.deltaTime;
+		if(this.delta > this.span) {
+			this.delta = 0; // 経過時間リセット
+			// Instantiateにprefab(設計図) を渡すとインスタンスが取れる
+			GameObject go = Instantiate (arrowPrefab) as GameObject;
+			int px = Random.Range (-6, 7);
+			go.transform.position = new Vector3 (px, 7, 0);
+	}
+}
